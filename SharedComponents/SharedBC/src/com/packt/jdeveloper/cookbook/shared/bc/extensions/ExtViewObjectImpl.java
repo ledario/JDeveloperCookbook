843d0b1a38dcbafd52fbe110c4e0a9d747c5501b
ledario <ledario@hotmail.com>
2014-11-07 11:55:51 -0500

    Chap 3 Recipe 3

--- SharedComponents/SharedBC/src/com/packt/jdeveloper/cookbook/shared/bc/extensions/ExtViewObjectImpl.java
+++ SharedComponents/SharedBC/src/com/packt/jdeveloper/cookbook/shared/bc/extensions/ExtViewObjectImpl.java
@@ -1,6 +1,18 @@
 package com.packt.jdeveloper.cookbook.shared.bc.extensions;
 
+import oracle.jbo.AttributeDef;
+import oracle.jbo.server.ViewAttributeDefImpl;
 import oracle.jbo.server.ViewObjectImpl;
 
 public class ExtViewObjectImpl extends ViewObjectImpl {
+    protected void setQueriable(int attribute, boolean condition) { 
+        // get the attribute definition
+        AttributeDef def = getAttributeDef(attribute);
+        // set/unset only if needed
+        if (def != null && def.isQueriable() != condition) { 
+            // set/unset queriable
+            ViewAttributeDefImpl attributeDef = (ViewAttributeDefImpl)def; 
+            attributeDef.setQueriable(condition);
+        } 
+    }
 }
